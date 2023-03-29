eaf4c4683b6305751e43dd49883d49d0c33e4bf9
hamsini.sankaran@berkeley.edu <hamsini.sankaran@berkeley.edu>
2023-03-28 23:48:23 -0700

    ding research proposal rmd file

--- data_cleaning.R
+++ data_cleaning.R
@@ -6,6 +6,7 @@ data_cleaning <- function() {
            release_date, budget, revenue, runtime, status,
            vote_average, vote_count, credits)
   movie_cleaned_df <- filter(movie_df, budget > 0, revenue > 0, (status %in% c("Released")),!(budget %in% c(5000000000, 800000000)))
+  
   # Return the preprocessed data frame
   return(movie_cleaned_df)
 }

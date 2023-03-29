2aa18fd04d72d8346da861c333a170609441a9ae
hamsini.sankaran@berkeley.edu <hamsini.sankaran@berkeley.edu>
2023-03-28 22:30:28 -0700

    data loading and data cleaning

--- data_cleaning.R
+++ data_cleaning.R
@@ -5,9 +5,7 @@ data_cleaning <- function() {
     select(id, title, genres, popularity, production_companies,
            release_date, budget, revenue, runtime, status,
            vote_average, vote_count, credits)
-  
-  movie_cleaned_df <- filter(movie_df, budget > 0, revenue > 0, !(budget %in% c(5000000000, 800000000)))
-  
+  movie_cleaned_df <- filter(movie_df, budget > 0, revenue > 0, (status %in% c("Released")),!(budget %in% c(5000000000, 800000000)))
   # Return the preprocessed data frame
   return(movie_cleaned_df)
 }
